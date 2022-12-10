815430efa75af84afba9ec5febe68ebbfbb97bb1
Jihun Jeong <demd7362@gmail.com>
2022-12-11 00:39:11 +0900

    Update DefaultMethodExample.java

--- DefaultMethodExample.java
+++ DefaultMethodExample.java
@@ -1,12 +1,16 @@
 interface Phone {
 	void call();
 	void message();
-	default void whenDrop() {
+	static void whenDrop() {
 		System.out.println("떨어트리면 망가집니다");
 	}
-	static void internet() {
+	default void internet() {
 		System.out.println("인터넷을 쓰면 돈이 엄청나게 빠져나갑니다.");
 	}
+	default void slide() {
+		System.out.println("슬라이드 폰입니다.");
+	}
+	
 }
 
 //  interface의 default 메소드
@@ -15,7 +19,7 @@ interface Phone {
 //
 //- 참조 변수로 함수를 호출할 수 있다.
 //
-//- implements한 클래스에서 재정의가 가능하다.
+//- 재정의가 가능하다.
 
 
 //  interface의 static 메소드 
@@ -30,25 +34,34 @@ class SmartPhone implements Phone {
 
 	@Override
 	public void call() {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public void message() {
-		// TODO Auto-generated method stub
+		
 		
 	}
-
-	
+	public void internet() {
+		System.out.println("인터넷을 쓰면 데이터 사용량이 빠져나갑니다.");
+	}
+	public void youtube() {
+		System.out.println("유튜브 재생이 가능합니다");
+		
+	}
 	
 }
 
 public class DefaultMethodExample {
 	public static void main(String[] args) {
 		Phone phone = new SmartPhone();
-		phone.whenDrop();
-		Phone.internet(); // 인스턴스 변수로 생성하면 찾을 수 없는 메소드
+		Phone.whenDrop(); // static 메소드 사용
+		phone.slide(); // default 메소드 사용
+		phone.internet(); // default 메소드 재정의. 이 경우 인터페이스의 internet() 메소드는 없는거나 마찬가지이다
+		
+		SmartPhone smartPhone = (SmartPhone)phone; // == SmartPhone smartPhone = new SmartPhone();
+		smartPhone.youtube();
 	}
 	
 }
